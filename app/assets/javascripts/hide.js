1ef48ef54b54662edf9403f4d52357b3ef91f7e7
Sean Oughton <seanoughton@gmail.com>
2018-07-09 22:50:49 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -2,6 +2,6 @@ const hideWhenClicked = function () {
   //bind #hide_this
   //const link = document.getElementById('hide_this')
   $('a').hide();
-  //console.log(this)
+  console.log(this)
   //$('this').parent().hide();
 }
